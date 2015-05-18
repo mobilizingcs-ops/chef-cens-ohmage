d897f4c1a18c44be930be514e83589870afd6d40
Steve Nolen <technolengy@gmail.com>
2015-05-18 13:25:29 -0700

    add mtest-specific config for nginx

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'technolengy@gmail.com'
 license 'Apache 2.0'
 description 'Installs/Configures cens-ohmage'
 long_description 'Installs/Configures cens-ohmage'
-version '0.0.11'
+version '0.0.12'
 
 %w(ubuntu).each do |os|
   supports os
